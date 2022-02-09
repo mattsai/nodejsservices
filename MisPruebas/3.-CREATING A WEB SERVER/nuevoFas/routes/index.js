46672a1d2510bdf3fbafec50e8cec754a75d9078
narmer <alvaro.narmer@ibm.com>
2022-02-08 23:22:32 -0600

    res.sendFile enpublic detectado

---
+++ MisPruebas/3.-CREATING A WEB SERVER/nuevoFas/routes/index.js
@@ -0,0 +1,7 @@
+module.exports = async function (fastify,options){
+    fastify.get('/',(req,res)=>{
+        // res.send('perra')
+        // res.sendFile('index.html')
+        // res.sendFile('hello.html')
+    })
+}
